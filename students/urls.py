1cfc98619cc6eefc584dfebab3c5670fcf64e3c4
NUPT-Pig <nupt_b09040212@sina.com>
2017-06-05 16:05:54 +0800

    update_destory_student

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from students.views import StudentsView
+from students.views import StudentsView, StudentDetailView
 
 urlpatterns = [
     url(r'^$', StudentsView.as_view(), name='list_create_students'),
+    url(r'detail/(?P<pk>\d+)/$', StudentDetailView.as_view(), name='detail_update_destory_student')
 ]
--- students/views.py
+++ students/views.py
@@ -25,3 +25,34 @@ class StudentsView(generics.ListCreateAPIView):
             print str(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return response
+
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Students.objects.all()
+    serializer_class = StudentsSerializer
+
+    def put(self, request, *args, **kwargs):
+        try:
+            response = super(StudentDetailView, self).put(request, *args, **kwargs)
+            obj = self.get_object()
+            if response.status_code == status.HTTP_200_OK:
+                user_serializer = UserSerializer(obj.user, data=request.data, partial=True)
+                if user_serializer.is_valid():
+                    user_serializer.save()
+                else:
+                    print user_serializer.errors
+        except Exception as e:
+            print str(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            obj = self.get_object()
+            if obj.user:
+                obj.user.delete()
+            response = super(StudentDetailView, self).delete(request, *args, **kwargs)
+        except Exception as e:
+            print str(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return response
