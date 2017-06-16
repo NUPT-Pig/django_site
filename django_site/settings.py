84ef0ec762aa0d2600b2b04ea5688287182909b8
NUPT-Pig <nupt_b09040212@sina.com>
2017-06-16 14:47:14 +0800

    add static urls

--- django_site/settings.py
+++ django_site/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/anshun/TEST/static/'
+STATIC_ROOT = '/home/anshun/python/django_site/django_site/static/'
 MEDIA_ROOT = '/home/anshun/TEST/media/'
 
 #logging
