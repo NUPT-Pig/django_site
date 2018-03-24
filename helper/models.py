f76fd46d0c6e0f91583cce8b9e492a9946fed528
NUPT-Pig <nupt_b09040212@sina.com>
2018-03-24 18:40:42 +0800

    account

--- helper/models.py
+++ helper/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, null=True)
+    money = models.FloatField(default=0)
+    comment = models.TextField(default="no comment")
+    valid = models.BooleanField(default=False)
+    check_list = models.CharField(max_length=5, null=True)
